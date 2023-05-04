b0cf396611a62e5ec568966656e5309dff4a3556
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-05-03 22:01:10 -0700

    finished code for porduct-routes and tag-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,16 +19,14 @@ router.get('/', async(req, res) => {
     res.status(500).json(err);
   }
 });
-
+// DOESNT WORK
 // get one product
 router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id,{
-      include: [{ model: Category }, 
-      { model: Tag, attributes: ['tag_name'], through: ProductTag, as: 'productTag_products'}
-      ]
+      include: [{ model: Category },]
     });
 
     res.status(200).json(productData);
@@ -111,8 +109,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try{
+    const productData = await Product.destroy({
+      where: {id: req.params.id,},
+    })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
